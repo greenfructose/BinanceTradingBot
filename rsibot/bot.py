08804f555302b419b61b3cf918c449c6b161130c
greenfructose <justinclarkturney@gmail.com>
2020-12-22 12:44:17 -0600

    Started caluclateing MACD to cross reference with RSI

--- rsibot/bot.py
+++ rsibot/bot.py
@@ -4,12 +4,12 @@ from binance.client import Client
 from binance.enums import *
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
-
 RSI_PERIOD = 14
 RSI_OVERBOUGHT = 70
 RSI_OVERSOLD = 30
 TRADE_SYMBOL = 'ETHUSD'
 TRADE_QUANTITY = 0.05
+
 
 closes = []
 in_position = False
@@ -42,20 +42,22 @@ def on_message(ws, message):
 
     print('received message')
     json_message = json.loads(message)
-    pprint.pprint(json_message)
+    pprint.pprint(json_message['k']['x'])
 
     candle = json_message['k']
 
     is_candle_closed = candle['x']
     close = candle['c']
-
+    print(f'Period: {len(closes)}')
     if is_candle_closed:
         print("candle closed at {}".format(close))
         closes.append(float(close))
         print("closes")
         print(closes)
-
         if len(closes) > RSI_PERIOD:
+            macd, sig, hist = talib.MACDFIX(closes)
+
+            print(f'MACD: {type(macd)}')
             np_closes = numpy.array(closes)
             rsi = talib.RSI(np_closes, RSI_PERIOD)
             print("all rsis calculated so far")
